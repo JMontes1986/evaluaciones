1f58024795244beb4c2900511baf18f2f7c3b976
Julian Montes Ramirez <damon.198623@gmail.com>
2025-08-11 00:55:49 +0000

    revisa cuando quiero entrar a dashbaord me devuelve a /login y no debe s

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/administracion/:path*", "/login", "/"],
+  matcher: ["/dashboard", "/dashboard/:path*", "/administracion/:path*", "/login", "/"],
 };
